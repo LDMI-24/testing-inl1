f82badd4d97db712ba13e10a47f2f46544fef304
LDMI-24 <lenita.dahlin@hotmail.com>
2026-01-02 00:31:20 +0100

    feat: add some functionality

---
+++ my-app/src/Shoppingcart.tsx
@@ -0,0 +1,27 @@
+
+
+
+export const Shoppingcart = () => {
+  //pseudocode to be fixed
+  /*
+  cart = [];
+  
+  cartTotaltExclTax = 0;
+  cartTax = 0;
+
+    constructor() {}
+  
+  addProduct(productId: number, quantity: number) {}
+  removeProduct(productId: number, quantity: number) {}
+  applyVoucher(code: string) {}
+  clearCart() {}
+  placeOrder(customerData) {}
+*/
+
+  return (
+    <>
+    <h1>Shoppingcart</h1>
+
+    </>
+  );
+};
