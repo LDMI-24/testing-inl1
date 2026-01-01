4a6f365c159da53c8289369af030b35824f393a3
LDMI-24 <lenita.dahlin@hotmail.com>
2026-01-01 22:36:26 +0100

    feat: add order form and styling

---
+++ my-app/src/OrderForm.tsx
@@ -0,0 +1,72 @@
+import './orderForm.css'
+
+export const OrderForm = () => {
+
+  return (
+    <>
+    <h1>Order Form</h1>
+      <form>
+        <div className="form-fields">
+          <div className="form-basic-info">
+            <label>
+              <span>First Name</span>
+              <input className="form-field" id="f-name" type="text" autoComplete="given-name" required />
+            </label>
+            <label>
+              <span>Last Name</span>
+              <input className="form-field" id="l-name" type="text" autoComplete="family-name" required />
+            </label>
+            <label>
+              <span>Streetname</span>
+              <input className="form-field" id="street-name" type="text" required />
+            </label>
+            <label>
+              <span>Postal code</span>
+              <input className="form-field" id="postal-code" type="number" required />
+            </label>
+            <label>
+              <span>City</span>
+              <input className="form-field" id="city" type="text" required />
+            </label>
+            <label>
+              <span>Door Code (if applicable)</span>
+              <input className="form-field" id="door-code" type="number" inputMode="numeric" />
+            </label>
+            <label>
+              <span>Your phonenumber (mobile)</span>
+              <input className="form-field" id="phone-number" type="tel" autoComplete="tel" required />
+            </label>
+            <label>
+              <span>Your Email</span>
+              <input className="form-field" id="email" type="email" autoComplete="on" required />
+            </label>
+          </div>
+          <div className="form-payment-type">
+            <input type="radio" className="form-field" id="invoice" name="payment-options" value="Pay with Invoice" />
+            <label htmlFor="invoice">Pay with Invoice</label>
+            <input type="radio" className="form-field" id="card" name="payment-options" value="Pay with Card" />
+            <label htmlFor="card">Pay with Card</label>
+            <div id="card-details" className="hidden">
+                <label>
+                    <span>Card Number</span>
+                    <input className="form-field" id="card-number" type="number" />
+                </label>
+                <label>
+                    <span>Card Expiration</span>
+                    <input className="form-field" id="card-expiration" type="number" />
+                </label>
+                <label>
+                    <span>Card CVC</span>
+                    <input className="form-field" id="card-cvc" type="number" />
+                </label>
+            </div>
+          </div>
+        </div>
+        <div className="order-buttons">
+          <input className="cancel-order-btn" type="reset" value="Cancel Order" />
+          <input className="place-order-btn" type="submit" value="Place Order" disabled />
+        </div>
+      </form>
+    </>
+  );
+};
